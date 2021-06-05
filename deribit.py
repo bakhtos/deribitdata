8fcda454dddc92fd272eaf32650d524603edc84e
bakhtos <alexander.bakhtin@tuni.fi>
2021-06-05 15:33:36 +0300

    Error handling

--- deribit.py
+++ deribit.py
@@ -7,7 +7,12 @@ while True:
 	params = {"currency" : "BTC", "kind" : "option"}
 	response = requests.get(endpoint, params = params)
 	response_json = response.json()
-	data[response_json['usOut']] = response_json["result"]
+	if "error" in response_json:
+		print(time.asctime(), "error when requesting files")
+		data[response_json['usOut']] = response_json["error"]
+	else:
+		print(time.asctime(), "data acquisition successful")
+		data[response_json['usOut']] = response_json["result"]
 	if(time.localtime().tm_hour == 23 and time.localtime().tm_min>45):
 		with open(time.asctime()+".json", "w") as fp:
 			json.dump(data, fp, indent = "\t")
