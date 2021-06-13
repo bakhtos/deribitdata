10e37a538f80289dce20c98154c37ef9b699e204
bakhtos <alexander.bakhtin@tuni.fi>
2021-06-13 16:03:56 +0300

    Break code into functions

--- deribit.py
+++ deribit.py
@@ -1,20 +1,31 @@
 import requests, json, time
 
-interval = 15*60
-data = dict()
-while True:
-	endpoint = "https://test.deribit.com/api/v2/public/get_book_summary_by_currency"
-	params = {"currency" : "BTC", "kind" : "option"}
-	response = requests.get(endpoint, params = params)
+ENDPOINT = "https://test.deribit.com/api/v2/public/get_book_summary_by_currency"
+INTERVAL = 15*60
+PARAMS_BTC = {"currency" : "BTC", "kind" : "option"}
+PARAMS_ETH = {"currency" : "ETH", "kind" : "option"}
+def get_data(params, data):
+	response = requests.get(ENDPOINT, params = params)
 	response_json = response.json()
 	if "error" in response_json:
-		print(time.asctime(), "error when requesting files")
+		print(time.asctime(), params["currency"], "error when requesting files")
 		data[response_json['usOut']] = response_json["error"]
 	else:
-		print(time.asctime(), "data acquisition successful")
+		print(time.asctime(), params["currency"], "data acquisition successful")
 		data[response_json['usOut']] = response_json["result"]
-	if(time.localtime().tm_hour == 23 and time.localtime().tm_min>45):
-		with open(time.asctime()+".json", "w") as fp:
-			json.dump(data, fp, indent = "\t")
-			data.clear()
-	time.sleep(interval)
+	
+def save_file(data, cur):
+	with open(cur+time.asctime()+".json", "w") as fp:
+		json.dump(data, fp, indent = "\t")
+		data.clear()
+
+if __name__ == "__main__":
+	data_btc = dict()
+	data_eth = dict()
+	while True:
+		get_data(PARAMS_BTC, data_btc)
+		get_data(PARAMS_ETH, data_eth)	
+		if(time.localtime().tm_hour == 15 and time.localtime().tm_min>45):
+			save_file(data_btc, "BTC ")
+			save_file(data_eth, "ETH ")
+		time.sleep(INTERVAL)
